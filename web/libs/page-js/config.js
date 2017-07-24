5855c0206173f28b373a9d2f28e4b95bb2ff5332
haoxianliang <517189672@qq.com>
2017-07-24 13:40:48 +0800

    主页添加限时插件&修改common.css

--- web/libs/page-js/config.js
+++ web/libs/page-js/config.js
@@ -2,8 +2,10 @@ require.config({
 	paths : {
 		jquery :　'../jquery/jquery-3.1.1',
 		swiper : '../swiper/swiper-3.4.2.jquery.min',
+		hxLimitTime : '../hxLimitTime/hx-limitTime',
 	},
 	shim : {
 		swiper : ['jquery'],
+		hxLimitTime : ['jquery'],
 	}
 });
--- web/libs/page-js/index.js
+++ web/libs/page-js/index.js
@@ -1,5 +1,5 @@
 require(['config'],function(){
-	require(['jquery','swiper'],function($){
+	require(['jquery','swiper','hxLimitTime'],function($){
 		var mySwiper = new Swiper('.swiper-container',{
 		    loop: true,
 			autoplay: 3000,
@@ -7,6 +7,19 @@ require(['config'],function(){
 			paginationClickable :true,
 			autoplayDisableOnInteraction : false,
   		});
+		$.ajax({
+			url:'http://10.3.134.228:8888/timeLimit',
+			dataType:'json',
+			type:'POST',
+			success:function(res){
+				//console.log(res.data);
+				if(res.status==true){
+					$('.limitTime').hxLimitTime({
+						data: res.data,
+					});
+				}
+			}
+		});
 		
 	});
 });
