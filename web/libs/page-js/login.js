0d81b5e9c907c7d2f1468f190182f76c694c945d
huimei123 <1009628609@qq.com>
2017-07-25 18:52:19 +0800

    主页

--- web/libs/page-js/login.js
+++ web/libs/page-js/login.js
@@ -15,12 +15,16 @@ require(['config'],function(){
 			$('#passwd').val("");		
 			
 		});
+
+		//登录
 		$('.login_btn').on('click',function(){
 			$.ajax({
 				url : 'http://10.3.134.228:8888/login',
 				type: 'POST',
 				success: function(res){
 					console.log(res);
+					// var data = res.data;
+					// if(res)
 				}
 			});
 		})
