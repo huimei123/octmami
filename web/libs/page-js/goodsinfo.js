6ca5e5e9ea13d32c5b397b0790bf5744139d8b34
haoxianliang <517189672@qq.com>
2017-07-27 21:10:03 +0800

    完善详情页功能

--- web/libs/page-js/goodsinfo.js
+++ web/libs/page-js/goodsinfo.js
@@ -13,6 +13,10 @@ require(['config'],function(){
 		var id = location.search.split('=')[1];
 		var obj = '';
 		var goodsArr =[];
+		var size = '';
+		var sizeNum = 0;
+		var color = '';
+		var colorNum = 0;
 		//console.log(id);
 		var number = 0;
 		if(localStorage.getItem('shoppingcar')==null){
@@ -46,7 +50,12 @@ require(['config'],function(){
 					//console.log(JSON.parse(obj))
 					carousel(res);
 					$('.info').html(goodObj.productName);
-					$('.price').html(goodObj.currentPrice);
+					if(goodObj.currentPrice.indexOf('.00')==-1){
+						//console.log(goodObj.currentPrice.indexOf('.00'));
+						$('.price').html(goodObj.currentPrice+'.00');
+					}else{
+						$('.price').html(goodObj.currentPrice);
+					}				
 					$('.detailTitle').html(goodObj.productName);
 
 				}
@@ -70,6 +79,27 @@ require(['config'],function(){
 	  				argument += `<p class="goodsArguments">${res.data[0].arguments[i]}</p>`
   				}
 			}
+			console.log(res.data[0].size);
+			if(res.data[0].size){
+				sizeNum = res.data[0].size.length*1;
+				for(var i=0;i<res.data[0].size.length;i++){
+					size += `<i class="sizeType">${res.data[0].size[i]}</i>`
+				}
+				$('.sizeBg').html('<div class="size">尺码:</div>'+size);
+				$('.sizeType').eq(0).addClass('active');
+			}else{
+				$('.sizeBg').hide();
+			}
+			if(res.data[0].color){
+				colorNum = res.data[0].color.length*1;
+				for(var i=0;i<res.data[0].color.length;i++){
+					color += `<i class="colorType">${res.data[0].color[i]}</i>`
+				}
+				$('.colorBg').html('<div class="color">颜色:</div>'+color);
+				$('.colorType').eq(0).addClass('active');
+			}else{
+				$('.colorBg').hide();
+			}
 			//console.log(html);
 			$div.html(html).appendTo($('.swiper-container'));			
 			var mySwiper = new Swiper('.swiper-container',{
@@ -87,18 +117,22 @@ require(['config'],function(){
 		
 		
 		
-	
+		//详情页面对象
 		var infoPage = {
 			$jia: $('.jia'),
 			$jian: $('.jian'),
 			$btn_buy: $('.btn_buy'),
 			$buy_now: $('.buy_now'),
 			$toTop: $('.toTop'),
+			$sizeBg:$('.sizeBg'),
+			$colorBg:$('.colorBg'),
 			init : function(){
+				//添加物品事件
 				this.$jia.on('click',function(){
 					$('.qty').val(Number($('.qty').val())+1);
 					$('.jian').removeClass('disable');
 				});
+				//减少物品事件
 				this.$jian.on('click',function(){
 					if($('.qty').val()==2){
 						console.log(111);
@@ -108,6 +142,7 @@ require(['config'],function(){
 						$('.qty').val(Number($('.qty').val())-1);
 					}			
 				});
+				//放入购物车点击事件
 				this.$btn_buy.on('click',function(){			
 					if(goodsArr.length<=0){
 						obj['qty'] = $('.qty').val()*1;
@@ -137,6 +172,27 @@ require(['config'],function(){
 					storage.setItem('shoppingcar',stingObj);
 					//console.log(JSON.parse(localStorage.getItem('shoppingcar')));
 				});
+				//立即购买事件
+				this.$buy_now.on('click',function(){
+					console.log(111);
+				})
+				//尺寸点击事件
+				this.$sizeBg.on('click','i',function(e){
+					//console.log($(e.target).index());
+					for(var i=1;i<=sizeNum;i++){
+						$('.sizeType').eq(i-1).removeClass('active');
+					}
+					$('.sizeType').eq($(e.target).index()*1-1).addClass('active');
+				});
+				//颜色点击事件
+				this.$colorBg.on('click','i',function(e){
+					//console.log($(e.target).index());
+					for(var i=1;i<=sizeNum;i++){
+						$('.colorType').eq(i-1).removeClass('active');
+					}
+					$('.colorType').eq($(e.target).index()*1-1).addClass('active');
+				});
+				//返回顶部监听
 				setInterval(function(){
 					//console.log($('body').scrollTop());
 					if($('body').scrollTop()>=1000){
@@ -148,11 +204,7 @@ require(['config'],function(){
 						$('.toTop').hide();
 					}
 				}.bind(this),1500);
-			}
-
-			
-		}
-
-		
+			}			
+		}		
 	});
 });
