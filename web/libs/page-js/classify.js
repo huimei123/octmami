a798fa7e5fd9763d23a2e2660e3b7a01c3558aa5
yehua <1204030083@qq.com>
2017-07-27 15:28:48 +0800

    修改列表页价格排序

--- web/libs/page-js/classify.js
+++ web/libs/page-js/classify.js
@@ -1,7 +1,14 @@
 require(['config'],function(){
 	require(['jquery'],function($){
-		$('li a span').on('click',function(){
-			
+		$('li a span').on('click',function(e){
+			e.preventDefault();
+			var url = $(this).text()
+			location.href='./goodsList.html?search=' + url;
+		})
+		$('li a').on('click',function(e){
+			e.preventDefault();
+			var url = $(this).text()
+			location.href='./goodsList.html?search=' + url;
 		})
 	})
 })
--- web/libs/page-js/goodsList.js
+++ web/libs/page-js/goodsList.js
@@ -6,98 +6,77 @@ require(['config'],function(){
 		// });
 
 		//获取URL的参数值
-		function　getURLParams(key){
-			let params = location.search.slice(1).split('&');
-			let res = '';
-			params.forEach(function(item){
-				let arr = item.split('=');
-				if(arr[0] == key){
-					res = arr[1];
-				}
-			});
-			return res;
-		}
+		
+		let params = location.search.slice(1).split('=');
+		let res = decodeURI(params[1]);
+		var html = '';
 		//获取URL地址传过来的搜索参数
-		var search = decodeURI(getURLParams('search'));
-		$('.header_title').html(search);
-
-		//生成列表页结构
+		$('.header_title').text(res);
+		console.log(res)
+		var tamp = $('.price').attr('data-tamp',1);
+		//请求匹配的数据
 		$.ajax({
 			url: toggle+'query/data' ,
 			type: 'POST',
 			data : {
-				key : search,
+				key : res,
 			},
 			success: function(res){
-				
-				var data = res.data;
-				var html = '';
-				html = data.map(function(item,idx){
-					return `
-						<li class="pd_new_list_detail">
-							<a href="./goodsinfo.html?id=${item._id}">
-								<div class="pd_show_s_img">
-									<img src="./libs/img/productImg/${data[idx].productImg[0]}" alt="" />
-								</div>
-								<div class="pd_new_list_info">
-									<p class="pd_show_list_name">
-										${item.productName}                            
-									</p>
-									<p class="new_icon_promotion">免邮</p>
-									<p class="pd_new_list_price">
-										<span class="newPrice">￥${item.currentPrice}</span>
-										<span class="oldPrice">￥${item.originPrice}</span>
-									</p>
-								</div>
-							</a>
-						</li>
-					`;
-				}).join('');
-				$('.pdShowList').append(html);
+				showGoods(res);	
+							
 			}
 		});
-		//升序
+		//按价格排序
 		$('.price').on('click',function(e){
 			e.preventDefault();
-			
+			//点击切换排序方式
+			if($('.price').attr('data-tamp') == 1){
+				$('.price').attr('data-tamp',-1);
+			}else{
+				$('.price').attr('data-tamp',1);
+			}
+			//请求数据
 			$.ajax({
 				url: toggle+'sort' ,
 				type:'post',
 				data :{
 					key: 'currentPrice',
-					num: 1,
+					num:$('.price').attr('data-tamp'),
 				},
 				success : function(res){
-					console.log(res);
-					var data = res.data;
-					$('.pdShowList').hide();
-					var html = '';
-					html = data.map(function(item,idx){
-						return `
-							<li class="pd_new_list_detail">
-								<a href="">
-									<div class="pd_show_s_img">
-										<img src="./libs/img/productImg/${data[idx].productImg[0]}" alt="" />
-									</div>
-									<div class="pd_new_list_info">
-										<p class="pd_show_list_name">
-											${item.productName}                            
-										</p>
-										<p class="new_icon_promotion">免邮</p>
-										<p class="pd_new_list_price">
-											<span class="newPrice">￥${item.currentPrice}</span>
-											<span class="oldPrice">￥${item.originPrice}</span>
-										</p>
-									</div>
-								</a>
-							</li>
-						`;
-					}).join('');
-					$('.updShowList').append(html);
+					//console.log(res);
+					$('.pdShowList').html('');
+					showGoods(res);	
 				},
 			});
 		});
 		
-
+		//封装生成列表页结构
+		function showGoods(res){
+			var data = res.data;
+			html = '';
+			html = data.map(function(item,idx){
+				return `
+					<li class="pd_new_list_detail">
+						<a href="./goodsinfo.html?id=${item._id}">
+							<div class="pd_show_s_img">
+								<img src="./libs/img/productImg/${data[idx].productImg[0]}" alt="" />
+							</div>
+							<div class="pd_new_list_info">
+								<p class="pd_show_list_name">
+									${item.productName}                            
+								</p>
+								<p class="new_icon_promotion">免邮</p>
+								<p class="pd_new_list_price">
+									<span class="newPrice">￥${item.currentPrice}</span>
+									<span class="oldPrice">￥${item.originPrice}</span>
+								</p>
+							</div>
+						</a>
+					</li>
+				`;
+			}).join('');
+			$('.pdShowList').append(html);
+		}
 	});
 });
